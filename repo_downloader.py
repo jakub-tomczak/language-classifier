250c627632a98c23675b7034477db33c5415a12f
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-08 11:37:08 +0200

    Fetch random repos.

--- repo_downloader.py
+++ repo_downloader.py
@@ -1,6 +1,11 @@
+import random
+import json
 import requests as req
+import os
 
 base_url = 'https://api.github.com/'
+oldest_request = 1000
+downloaded_repos_dir = 'downloaded'
 
 def make_request(request, parameters = None):
     return req.get(base_url + request, params=parameters).json()
@@ -8,4 +13,12 @@ def make_request(request, parameters = None):
 def get_random_repositories_info():
     print(make_request('repos/jakub-tomczak/language-classifier/languages'))
 
+def get_public_repos(page=None):
+    if page == None:
+        page = random.randrange(0, oldest_request)
+        print('fetching repos from page %d' % page)
+    request = make_request('repositories', parameters={'since':page})
+    for data in request:
+        print(data['html_url'])
+get_public_repos()
 get_random_repositories_info()
